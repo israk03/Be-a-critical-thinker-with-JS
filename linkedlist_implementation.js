ced526bdc2b4b0b642cdeed0cfbe57652e5867b3
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-11-06 17:25:30 +0600

    insert

--- linkedlist_implementation.js
+++ linkedlist_implementation.js
@@ -43,6 +43,36 @@ class LinkedList {
     this.length++;
   }
 
+  //* insert at any position
+  insert(index, value) {
+    // if index is 0, prepend
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+
+    // if index is greater than length, append
+    if (index >= this.length) {
+      this.append(value);
+      return;
+    }
+
+    // insert at given index
+    const newNode = new Node(value);
+
+    let previousNode = this.head;
+
+    for (let i = 0; i < index - 1; i++) {
+      previousNode = previousNode.next;
+    }
+
+    // relink nodes
+    newNode.next = previousNode.next;
+    previousNode.next = newNode;
+
+    this.length++;
+  }
+
   // print the linked list
   print() {
     const arr = [];
@@ -65,4 +95,7 @@ myLinkedList.print();
 myLinkedList.prepend(5);
 myLinkedList.prepend(6);
 myLinkedList.prepend(7);
+myLinkedList.print();
+
+myLinkedList.insert(3, 15);
 myLinkedList.print();
